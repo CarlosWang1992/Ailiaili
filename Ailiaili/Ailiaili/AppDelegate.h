fa8f4386f00a0768869b7877e8a9207e065b7a96
carcarlos <carcarlos@163.com>
2017-02-15 23:19:32 +0800

    game start!

---
+++ Ailiaili/Ailiaili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ailiaili
+//
+//  Created by 汪成刚 on 2017/2/15.
+//  Copyright © 2017年 com.morethancoding.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ailiaili/Ailiaili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ailiaili
+//
+//  Created by 汪成刚 on 2017/2/15.
+//  Copyright © 2017年 com.morethancoding.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
